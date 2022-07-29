6a924691c12771a28cdf2bc46d368db87ad9cb68
Daniel Tilbey <daniel@sableproduction.com>
2022-07-29 13:52:12 +0100

    Town got spirited away
    - Bug: After I essentially had completed the town area to a moderate degree, I began work on terraforming some of the pathing areas like the beach and the forest. soon after all the placed objects had vanished. This essentially leaves me to have to build the town area again.

--- custom/custom_editor_model_preview_icon.py
+++ custom/custom_editor_model_preview_icon.py
@@ -2,4 +2,4 @@
 _reload_all = True
 
 
-data = {134227677: {'path': 'UP1ModelPreview/134227677', 'key': 134227677, 'size': '180X100'}, 134221373: {'path': 'UP1ModelPreview/134221373', 'key': 134221373, 'size': '180X100'}, 134276501: {'path': 'UP1ModelPreview/134276501', 'key': 134276501, 'size': '180X100'}}
+data = {134227677: {'path': 'UP1ModelPreview/134227677', 'key': 134227677, 'size': '180X100'}, 134221373: {'path': 'UP1ModelPreview/134221373', 'key': 134221373, 'size': '180X100'}, 134276501: {'path': 'UP1ModelPreview/134276501', 'key': 134276501, 'size': '180X100'}, 134247219: {'path': 'UP1ModelPreview/134247219', 'key': 134247219, 'size': '180X100'}, 134241227: {'path': 'UP1ModelPreview/134241227', 'key': 134241227, 'size': '180X100'}, 134219902: {'path': 'UP1ModelPreview/134219902', 'key': 134219902, 'size': '180X100'}, 134230071: {'path': 'UP1ModelPreview/134230071', 'key': 134230071, 'size': '180X100'}, 134279005: {'path': 'UP1ModelPreview/134279005', 'key': 134279005, 'size': '180X100'}}
