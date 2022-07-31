e289b876fbe5cbe7f9e7489c43cbb8e22e7aaae0
Daniel Tilbey <daniel@sableproduction.com>
2022-07-31 20:16:49 +0100

    Demo Polish
    Added some npcs just as placeholder, they exist but you can't talk to em. Improved the follower movement so it looks a lot better and added more pathing to some creatures to make things look a bit more alive and interesting

--- custom/custom_editor_icon.py
+++ custom/custom_editor_icon.py
@@ -1,4 +1,5 @@
 # -*- encoding:utf-8 -*-
 _reload_all = True
 
-data = {}
+
+data = {134229457: {'path': 'ui/134229457', 'describe': '', 'name': u'Pasted image 20220725130134', 'key': 134229457, 'uid': '134229457', 'size': '3840X2160'}}
